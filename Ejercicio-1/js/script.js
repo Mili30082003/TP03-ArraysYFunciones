baa3058084304c6f9fbeb3bfe65f5ad67cd4760a
Mili Antoni <miliantoni12@gmail.com>
2024-07-16 17:50:26 -0300

    EjERCICIO 1

---
+++ Ejercicio-1/js/script.js
@@ -0,0 +1,22 @@
+const meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+];
+
+document.write ('<h2> Lista de Meses</h2>');
+document.write ('<ul>');
+
+for (let i = 0; i < meses.length; i++){
+    document.write(`<li>${meses[i]}</li>`);
+}
+document.write('</ul>');
